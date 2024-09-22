68edf6310b934fbf4a78290c8e33311af88be2e1
= <=>
2024-09-22 17:59:11 -0600

    fixed index.php las tarjetas y sus contenedores

--- Frontend/index.php
+++ Frontend/index.php
@@ -35,89 +35,85 @@ $tipoAlojamiento = TipoAlojamiento::obtenerTiposAlojamientos($db);
 </head>
 
 <body>
-  <nav class="navbar navbar-expand-lg bg-body-tertiary">
-    <div class="container-fluid">
-      <a class="navbar-brand" href="#">Navbar</a>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-          <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="#">Home</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Link</a>
-          </li>
-          <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-              Dropdown
-            </a>
-            <ul class="dropdown-menu">
-              <li><a class="dropdown-item" href="#">Action</a></li>
-              <li><a class="dropdown-item" href="#">Another action</a></li>
-              <li>
-                <hr class="dropdown-divider">
-              </li>
-              <li><a class="dropdown-item" href="#">Something else here</a></li>
-            </ul>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link disabled" aria-disabled="true">Disabled</a>
-          </li>
-        </ul>
+  <div class="container-fluid pb-5">
+    <nav class="navbar navbar-expand-lg bg-body-tertiary fixed-top">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#">Navbar</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="#">Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Link</a>
+            </li>
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                Dropdown
+              </a>
+              <ul class="dropdown-menu">
+                <li><a class="dropdown-item" href="#">Action</a></li>
+                <li><a class="dropdown-item" href="#">Another action</a></li>
+                <li>
+                  <hr class="dropdown-divider">
+                </li>
+                <li><a class="dropdown-item" href="#">Something else here</a></li>
+              </ul>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+            </li>
+          </ul>
 
 
-        <div class="search-container me-3">
-          <form class="d-flex" role="search">
-            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-            <button class="btn btn-outline-success" type="submit">Search</button>
-          </form>
-        </div>
+          <div class="search-container me-3">
+            <form class="d-flex" role="search">
+              <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+              <button class="btn btn-outline-success" type="submit">Search</button>
+            </form>
+          </div>
 
-        <div class="login-container">
-          <div class="d-flex">
-            <a href="signUp.php" class="btn btn-primary">Log in</a>
+          <div class="login-container">
+            <div class="d-flex">
+              <a href="signUp.php" class="btn btn-primary">Log in</a>
+            </div>
           </div>
         </div>
-
-
-
       </div>
-    </div>
-  </nav>
+    </nav>
+  </div>
 
   <!--lista de alojamientos -->
 
-  <div class="container">
-    <div class="row">
-      <div class="col-12">
-        <h1 class="text-center">Alojamientos</h1>
-      </div>
+  <div class="container-fluid pt-5">
+    <div class="col-12">
+      <h1 class="text-center">Alojamientos</h1>
     </div>
-    <!-- Fila para las tarjetas -->
-     
-  <?php  
-  foreach ($alojamiento as $aloja) { ?>
-  <div class="col-4">
+    <div class="row">
+        <!-- Fila para las tarjetas -->
+        <?php
+        foreach ($alojamiento as $aloja) { ?>
+          <div class="col-4">
+            <div class="card">
+              <img src="<?php echo $aloja['imagen']; ?>" class="card-img-top" alt="...">
+              <div class="card-body">
+                <h5 class="card-title"><?php echo $aloja['nombre_alojamiento']; ?></h5>
+                <p class="card-location"><strong>Ubicación:</strong> <?php echo $aloja['ubicacion']; ?></p>
+                <p class="card-text"><?php echo $aloja['descripcion']; ?></p>
+                <p class="card-text"><?php echo $aloja['tipo_alojamiento']; ?></p>
+                <p class="card-price"><strong>Precio:</strong> $<?php echo $aloja['precio']; ?> por noche</p>
+                <p class="card-availability"><strong>Estado:</strong> <?php echo $aloja['estado_alojamiento']; ?></p>
+                <a href="reservas.php" class="btn btn-primary">Reservar</a>
+              </div>
+            </div>
+          </div>
+        <?php } ?>
+    </div>
 
-<div class="card">
-  <img src="<?php echo $aloja['imagen']; ?>" class="card-img-top" alt="...">
-  <div class="card-body">
-    <h5 class="card-title"><?php echo $aloja['nombre_alojamiento']; ?></h5>
-    <p class="card-location"><strong>Ubicación:</strong> <?php echo $aloja['ubicacion']; ?></p>
-    <p class="card-text"><?php echo $aloja['descripcion']; ?></p>
-    <p class="card-text"><?php echo $aloja['tipo_alojamiento']; ?></p>
-    <p class="card-price"><strong>Precio:</strong> $<?php echo $aloja['precio']; ?> por noche</p>
-    <p class="card-availability"><strong>Estado:</strong> <?php echo $aloja['estado_alojamiento']; ?></p>
-    <a href="reservas.php" class="btn btn-primary">Reservar</a>
-  </div>
-</div>
-</div>
-
-<?php }?>
-
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
 
 </body>
 
